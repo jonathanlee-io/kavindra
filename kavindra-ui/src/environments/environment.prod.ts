d538a9526aef8e23bab233ea12b340d4c1ce0425
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-11-28 19:00:56 +0000

    Update FLAGSMITH_CLIENT_SDK_KEY in production config
    The FLAGSMITH_CLIENT_SDK_KEY in the production environment configuration has been updated to a new value. This change is essential for maintaining connectivity and functionality with the Flagsmith service. Ensure all dependent services and environments are aware of this update to prevent integration issues.

--- kavindra-ui/src/environments/environment.prod.ts
+++ kavindra-ui/src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
   SUPABASE_URL: 'https://auth.kavindra.io',
   SUPABASE_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucGVmeXNvdmV3Zm5hdmV1a2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1NjY3NDUsImV4cCI6MjA0NTE0Mjc0NX0.cyRHfAwWrSTiUtR80FYHJ0eCPFJgPKz3FdNo4UdVKac',
-  FLAGSMITH_CLIENT_SDK_KEY: 'eJimRjGPKUcAvhmBxPtHVX',
+  FLAGSMITH_CLIENT_SDK_KEY: '6zrfkbuWjUXwyNTKdFWqPD',
   FLAGSMITH_API_URL: 'https://flagsmith.kavindra.io/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
   APPLICATION_MESSAGES_SERVICE_BASE_URL: 'v1/application-messages',
