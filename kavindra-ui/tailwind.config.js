afd2ca00a61c1302886498e8c86ccace4bcf63bb
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-11-28 20:25:24 +0000

    Add consistent blue theme color across components
    Introduced `light-blue` and `dark-blue` theme colors in Tailwind configuration and applied them to matched elements across several component templates. This change aims to ensure visual coherence and improve the aesthetic consistency of the user interface.

--- kavindra-ui/tailwind.config.js
+++ kavindra-ui/tailwind.config.js
@@ -10,10 +10,12 @@ module.exports = {
       'light-primary': '#f1f4f8',
       'light-secondary': '#64737a',
       'light-text-primary': '#000000',
+      'light-blue': '#007bff',
       /* DARK THEME */
       'dark-primary': '#001631',
       'dark-secondary': '#596570',
       'dark-text-primary': '#FFFFFF',
+      'dark-blue': '#007bff',
     }
   },
   darkMode: 'class',
