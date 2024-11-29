94440b93cb14731d8a5a4b9416084b340277a7fa
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-11-29 16:24:31 +0000

    Add subdomain validation and feedback UI components
    Integrated form validation and feedback for subdomain input on the create project page. The UI now provides dynamic feedback using a form control with distinct visual cues for valid, invalid, loading, and available states. New color classes and icons were added to enhance user experience with clear success and error messaging.

--- kavindra-ui/src/app/components/pages/_authenticated/_create_client_pages/create-project-page/create-project-page.component.ts
+++ kavindra-ui/src/app/components/pages/_authenticated/_create_client_pages/create-project-page/create-project-page.component.ts
@@ -1,13 +1,19 @@
+import {NgClass, NgIf, NgSwitch, NgSwitchCase} from '@angular/common';
 import {Component} from '@angular/core';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {RouterLink} from '@angular/router';
 import {ButtonModule} from 'primeng/button';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {debounceTime, tap} from 'rxjs';
 
 import {RoutePath} from '../../../../../app.routes';
 import {rebaseRoutePath} from '../../../../../util/router/Router.utils';
 
+export type SubdomainState = 'INIT' | 'AVAILABLE' | 'UNAVAILABLE' | 'LOADING';
+
 @Component({
   selector: 'app-create-project-page',
-  imports: [RouterLink, ButtonModule],
+  imports: [RouterLink, ButtonModule, NgClass, ReactiveFormsModule, NgIf, ProgressSpinnerModule, NgSwitchCase, NgSwitch],
   standalone: true,
   templateUrl: './create-project-page.component.html',
   styleUrl: './create-project-page.component.scss',
@@ -15,4 +21,32 @@ import {rebaseRoutePath} from '../../../../../util/router/Router.utils';
 export class CreateProjectPageComponent {
   protected readonly rebaseRoutePath = rebaseRoutePath;
   protected readonly RoutePath = RoutePath;
+  subdomainFormControl = new FormControl<string>('', Validators.compose([
+    Validators.required,
+    Validators.pattern(/^[a-zA-Z0-9][a-zA-Z0-9.-]+[a-zA-Z0-9]$/),
+  ]),
+  );
+  subdomainState: SubdomainState = 'INIT';
+
+  constructor() {
+    this.subdomainFormControl.valueChanges.pipe(
+        tap(() => {
+          this.subdomainState = 'LOADING';
+        }),
+        debounceTime(500),
+        tap(() => {
+          if (this.subdomainFormControl.invalid) {
+            this.subdomainState = 'UNAVAILABLE';
+            return;
+          }
+          if (this.subdomainState === 'LOADING') {
+            this.subdomainState = 'AVAILABLE';
+            return;
+          }
+          setTimeout(() => {
+            this.subdomainState = 'UNAVAILABLE';
+          }, 5000);
+        }),
+    ).subscribe();
+  }
 }
--- kavindra-ui/tailwind.config.js
+++ kavindra-ui/tailwind.config.js
@@ -11,11 +11,15 @@ module.exports = {
       'light-secondary': '#64737a',
       'light-text-primary': '#000000',
       'light-blue': '#007bff',
+      'light-error': '#dc3545',
+      'light-success': '#28a745',
       /* DARK THEME */
       'dark-primary': '#001631',
       'dark-secondary': '#596570',
       'dark-text-primary': '#FFFFFF',
       'dark-blue': '#007bff',
+      'dark-error': '#dc3545',
+      'dark-success': '#28a745',
     }
   },
   darkMode: 'class',
