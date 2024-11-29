77e4148d3d15fe8377de4407f2865a770efce840
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-11-29 13:30:20 +0000

    Format import statements consistently
    Adjusted the formatting of import statements across the `create-client-intro-page` component and its spec file to ensure consistency with coding standards. This change does not affect functionality but improves readability and maintainability of the codebase.

--- kavindra-ui/src/app/components/pages/_authenticated/_create_client_pages/create-client-intro-page/create-client-intro-page.component.spec.ts
+++ kavindra-ui/src/app/components/pages/_authenticated/_create_client_pages/create-client-intro-page/create-client-intro-page.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CreateClientIntroPageComponent } from './create-client-intro-page.component';
+import {CreateClientIntroPageComponent} from './create-client-intro-page.component';
 
 describe('CreateClientIntroPageComponent', () => {
   let component: CreateClientIntroPageComponent;
@@ -8,9 +8,9 @@ describe('CreateClientIntroPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateClientIntroPageComponent]
+      imports: [CreateClientIntroPageComponent],
     })
-    .compileComponents();
+        .compileComponents();
 
     fixture = TestBed.createComponent(CreateClientIntroPageComponent);
     component = fixture.componentInstance;
--- kavindra-ui/src/app/components/pages/_authenticated/_create_client_pages/create-client-intro-page/create-client-intro-page.component.ts
+++ kavindra-ui/src/app/components/pages/_authenticated/_create_client_pages/create-client-intro-page/create-client-intro-page.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-create-client-intro-page',
   imports: [],
   templateUrl: './create-client-intro-page.component.html',
-  styleUrl: './create-client-intro-page.component.scss'
+  styleUrl: './create-client-intro-page.component.scss',
 })
 export class CreateClientIntroPageComponent {
 
