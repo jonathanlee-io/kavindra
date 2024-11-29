742bdd1b2166faeb5e08da897df525d7f4b8a305
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-11-29 15:42:11 +0000

    Integrate PrimeNG ButtonModule in create project page
    Added PrimeNG ButtonModule to enhance UI consistency and simplify button management in the create project page. Replaced the native button element with a PrimeNG button to leverage enhanced styling and disabling features. This change facilitates future modification and consistency across the application.

--- kavindra-ui/src/app/components/pages/_authenticated/_create_client_pages/create-project-page/create-project-page.component.ts
+++ kavindra-ui/src/app/components/pages/_authenticated/_create_client_pages/create-project-page/create-project-page.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {ButtonModule} from 'primeng/button';
 
 import {RoutePath} from '../../../../../app.routes';
 import {rebaseRoutePath} from '../../../../../util/router/Router.utils';
 
 @Component({
   selector: 'app-create-project-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonModule],
   standalone: true,
   templateUrl: './create-project-page.component.html',
   styleUrl: './create-project-page.component.scss',
