1a86fa959cdca2a69ec4902d23f80e0477216950
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-11-29 22:43:45 +0000

    Simplify import statements formatting in test specs.
    Remove unnecessary spaces from import statements in both the MainDashboardComponent and ProjectsService test specification files. This change aims to keep the code style consistent and improve readability.

--- kavindra-ui/src/app/components/pages/_authenticated/_dashboard/main-dashboard/main-dashboard.component.spec.ts
+++ kavindra-ui/src/app/components/pages/_authenticated/_dashboard/main-dashboard/main-dashboard.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MainDashboardComponent } from './main-dashboard.component';
+import {MainDashboardComponent} from './main-dashboard.component';
 
 describe('MainDashboardComponent', () => {
   let component: MainDashboardComponent;
@@ -8,9 +8,9 @@ describe('MainDashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainDashboardComponent]
+      imports: [MainDashboardComponent],
     })
-    .compileComponents();
+        .compileComponents();
 
     fixture = TestBed.createComponent(MainDashboardComponent);
     component = fixture.componentInstance;
