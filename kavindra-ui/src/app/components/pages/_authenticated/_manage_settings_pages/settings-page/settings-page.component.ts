2d570d0ef3809f15fa6711bf6cf4571ea31f8c3c
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-11-29 15:23:58 +0000

    Refactor import statements for style consistency
    Updated import statements to follow consistent formatting across the SettingsPageComponent and its spec file. This change enhances readability and maintains uniformity in the codebase.

--- kavindra-ui/src/app/components/pages/_authenticated/_manage_settings_pages/settings-page/settings-page.component.ts
+++ kavindra-ui/src/app/components/pages/_authenticated/_manage_settings_pages/settings-page/settings-page.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-settings-page',
   imports: [],
   templateUrl: './settings-page.component.html',
-  styleUrl: './settings-page.component.scss'
+  styleUrl: './settings-page.component.scss',
 })
 export class SettingsPageComponent {
 
