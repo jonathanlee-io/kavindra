6892452c8bfce3449af545057ebb09f55d98c625
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-02 21:13:10 +0000

    Refactor import and indentation formatting.
    Adjusted import statements to match the project's coding style by removing spaces within braces. Also, fixed the indentation for the `.compileComponents()` method to enhance code readability and maintain consistency.

--- kavindra-ui/src/app/components/pages/_authenticated/_dashboard/client-dashboard/client-dashboard.component.spec.ts
+++ kavindra-ui/src/app/components/pages/_authenticated/_dashboard/client-dashboard/client-dashboard.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ClientDashboardComponent } from './client-dashboard.component';
+import {ClientDashboardComponent} from './client-dashboard.component';
 
 describe('ClientDashboardComponent', () => {
   let component: ClientDashboardComponent;
@@ -8,9 +8,9 @@ describe('ClientDashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ClientDashboardComponent]
+      imports: [ClientDashboardComponent],
     })
-    .compileComponents();
+        .compileComponents();
 
     fixture = TestBed.createComponent(ClientDashboardComponent);
     component = fixture.componentInstance;
