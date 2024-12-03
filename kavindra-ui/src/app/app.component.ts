f4154e708b69df763b42fd12d9edbb20895c3c1a
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-03 18:54:55 +0000

    Add ExampleComponent and clean up AppComponent code
    Introduced the ExampleComponent with HTML, TypeScript, and test specifications to enhance the application structure and functionality. Cleaned up AppComponent by removing unused services and variables for more efficient code maintenance. Updated package dependencies to include "@angular/elements".

--- kavindra-ui/src/app/app.component.ts
+++ kavindra-ui/src/app/app.component.ts
@@ -9,11 +9,9 @@ import {ToastModule} from 'primeng/toast';
 import {filter, tap} from 'rxjs';
 
 import {UserAuthenticationStore} from './+state/auth/user-auth.store';
-import {UserPreferencesStore} from './+state/user-preferences/user-preferences.store';
 import {AppService} from './app.service';
 import {NavbarComponent} from './components/lib/_navbar/navbar/navbar.component';
 import {FooterComponent} from './components/lib/footer/footer.component';
-import {AuthService} from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -34,24 +32,10 @@ import {AuthService} from './services/auth/auth.service';
 export class AppComponent implements OnInit {
   title = 'Kavindra';
   isSidebarVisible = signal<boolean>(false);
-  isBottomMessageVisible = signal<boolean>(true);
-  onAccept: () => void = () => {
-    this.isBottomMessageVisible.set(false);
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Info',
-      detail: 'Thank you for acknowledging!',
-      life: 3000,
-    });
-  };
-
-  private readonly authService = inject(AuthService);
   private readonly userAuthenticationStore = inject(UserAuthenticationStore);
-  private readonly userPreferencesStore = inject(UserPreferencesStore);
 
   constructor(
     private readonly config: PrimeNGConfig,
-    private readonly messageService: MessageService,
     private readonly router: Router,
     private readonly appService: AppService,
   ) {
---
+++ kavindra-ui/src/app/components/widget/example/example.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  imports: [],
+  templateUrl: './example.component.html',
+  styleUrl: './example.component.scss',
+})
+export class ExampleComponent {
+
+}
