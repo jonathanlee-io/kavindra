54be45b25222ec17caf2a63625b7ba9653a8d88c
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-03 23:06:55 +0000

    Simplify widget script retrieval logic
    Remove redundant environment check and realpathSync call from getWidgetScript function. Directly read widget.js script for all environments, assuming consistent file structure.

--- kavindra-api/src/app/projects/services/projects/projects.service.ts
+++ kavindra-api/src/app/projects/services/projects/projects.service.ts
@@ -121,18 +121,9 @@ export class ProjectsService {
   }
 
   async getWidgetScript() {
-    return fs.realpathSync('.');
-
-    if (
-      this.configService.getOrThrow<NodeEnvironment>('NODE_ENV') ===
-      'development'
-    ) {
-      return fs.readFileSync(
-        path.join(__dirname, '../../../../..', 'js-widget/dist/widget.js'),
-        'utf8',
-      );
-    }
-
-    // return fs.readFileSync('/dist/js-widget/dist/widget.js', 'utf8');
+    return fs.readFileSync(
+      path.join(__dirname, '../../../../..', 'js-widget/dist/widget.js'),
+      'utf8',
+    );
   }
 }
