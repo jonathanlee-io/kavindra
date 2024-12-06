b983ba3e87e5eb3ee389dea7f3a22d7ff2938979
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-06 22:06:03 +0000

    Add Tailwind config and update Yarn dependencies
    This commit adds a new Tailwind CSS configuration file to the kavindra-api directory, facilitating custom styling for the project. It also updates the yarn.lock file with additional dependencies to support new PostCSS plugins, providing enhanced CSS functionality.

--- kavindra-api/.eslintrc.js
+++ kavindra-api/.eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'widget/**/*', 'dist/**/*', 'node_modules/**/*', '**/*.d.ts', 'webpack.config.js'],
+  ignorePatterns: ['.eslintrc.js', 'widget/**/*', 'dist/**/*', 'node_modules/**/*', '**/*.d.ts', 'webpack.config.js', 'postcss.config.js', 'tailwind.config.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
---
+++ kavindra-api/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./widget/dist/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
