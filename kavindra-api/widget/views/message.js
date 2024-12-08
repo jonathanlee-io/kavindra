3084cdd88f7d10d1906f47c408a83cb0a0913160
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-08 17:26:19 +0000

    Remove console log statements from modal functions
    Cleaning up console log statements in the 'closeModal' and 'openModal' functions improves code readability and performance. These logs were likely used for debugging purposes and are no longer needed in the production code.

--- kavindra-api/widget/views/message.js
+++ kavindra-api/widget/views/message.js
@@ -5,13 +5,11 @@ const elements = [];
 let body;
 
 export function closeModal() {
-  console.log('Closing modal');
   const modal = document.getElementsByClassName('js-widget-modal')[0];
   modal.style.visibility = 'hidden';
 }
 
 export function openModal() {
-  console.log('Opening modal');
   const modal = document.getElementsByClassName('js-widget-modal')[0];
   modal.style.visibility = 'visible';
   const modalCloseButton = document.getElementById('js-widget-modal-close-button');
