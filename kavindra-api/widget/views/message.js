c6e43f0f983e667b0a4aa717d32cb06afaf228df
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-09 23:50:04 +0000

    Refactor modals and enhance feedback functionality
    The modal system has been updated to include distinct functionalities for menu and bug report interactions, allowing more precise user feedback collection. Styling adjustments were made to improve the visibility and responsiveness of interface components. The updated modals support more detailed user actions like reporting bugs and feature requests directly through the interface.

--- kavindra-api/widget/views/message.js
+++ kavindra-api/widget/views/message.js
@@ -4,16 +4,34 @@ import './message.css';
 const elements = [];
 let body;
 
-export function closeModal() {
-  const modal = document.getElementsByClassName('js-widget-modal')[0];
-  modal.style.visibility = 'hidden';
+export function closeModalMenu() {
+  const modal = document.getElementsByClassName('js-widget-modal-menu')[0];
+  modal.style.display = 'none'
 }
 
-export function openModal() {
-  const modal = document.getElementsByClassName('js-widget-modal')[0];
-  modal.style.visibility = 'visible';
-  const modalCloseButton = document.getElementById('js-widget-modal-close-button');
-  modalCloseButton.addEventListener('click', closeModal);
+export function openModalMenu() {
+  const modal = document.getElementsByClassName('js-widget-modal-menu')[0];
+  modal.style.display = 'block';
+  const modalCloseButton = document.getElementById('js-widget-modal-menu-close-button');
+  setTimeout(() => {
+    modalCloseButton.addEventListener('click', closeModalMenu);
+  }, 500);
+  const modalReportButton = document.getElementById('js-widget-modal-menu-bug-button');
+  modalReportButton.addEventListener('click', openModalBugReport);
+}
+
+export function closeModalBugReport() {
+  const modal = document.getElementsByClassName('js-widget-modal-bug-report')[0]
+  modal.style.display = 'none';
+  openModalMenu()
+}
+
+export function openModalBugReport() {
+  closeModalMenu();
+  const modal = document.getElementsByClassName('js-widget-modal-bug-report')[0]
+  modal.style.display = 'block';
+  const modalCloseButton = document.getElementById('js-widget-modal-bug-report-close-button');
+  modalCloseButton.addEventListener('click', closeModalBugReport);
 }
 
 export function show(text) {
@@ -32,6 +50,6 @@ export function show(text) {
   const widgetOverlay = document.getElementsByClassName('js-widget-overlay')[0];
   const widgetDialog = document.getElementsByClassName('js-widget-dialog')[0];
 
-  widgetOverlay.addEventListener('click', openModal);
-  widgetDialog.addEventListener('click', openModal);
+  widgetOverlay.addEventListener('click', openModalMenu);
+  widgetDialog.addEventListener('click', openModalMenu);
 }
