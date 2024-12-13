ed90797a6d3eb05567f11bf14849de66e3228d1e
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-13 00:41:25 +0000

    Add new dependencies and update existing ones in yarn.lock
    Integrates new packages such as @ngneat/spectator, jquery, and replace-in-file, while updating existing dependencies like aria-query and is-array-buffer. These changes ensure compatibility and introduce enhancements for test utilities and DOM manipulation.

--- kavindra-ui/src/app/components/pages/_login_register_pages/shared-account-page/shared-account-page.component.spec.ts
+++ kavindra-ui/src/app/components/pages/_login_register_pages/shared-account-page/shared-account-page.component.spec.ts
@@ -1,6 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {provideRouter} from '@angular/router';
 import {faker} from '@faker-js/faker/locale/en';
+import {createRoutingFactory, Spectator} from '@ngneat/spectator';
 import {render} from '@testing-library/angular';
 import {screen} from '@testing-library/dom';
 
@@ -11,21 +12,15 @@ import {testProviders} from '../../../../../test-util/test-providers.helper';
 describe('SharedAccountPageComponent', () => {
   let mockUserAuthenticationStore: { isLoading: jasmine.Spy<jasmine.Func>; };
 
-  beforeEach(async () => {
-    mockUserAuthenticationStore = {
-      isLoading: jasmine.createSpy('isLoading').and.returnValue(false),
-    };
-  });
-
-  afterEach(() => {
-    TestBed.resetTestingModule();
-  });
-
   describe('Unit Tests', () => {
     let component: SharedAccountPageComponent;
     let fixture: ComponentFixture<SharedAccountPageComponent>;
 
     beforeEach(async () => {
+      mockUserAuthenticationStore = {
+        isLoading: jasmine.createSpy('isLoading').and.returnValue(false),
+      };
+
       await TestBed.configureTestingModule({
         imports: [SharedAccountPageComponent],
         providers: [
@@ -49,8 +44,9 @@ describe('SharedAccountPageComponent', () => {
     });
   });
 
-  describe('Integration Tests', () => {
+  describe('@testing-library Integration Tests', () => {
     it('should render component with heading text the same as input', async () => {
+      mockUserAuthenticationStore = {isLoading: jasmine.createSpy('isLoading').and.returnValue(false)};
       const headingText = faker.lorem.words();
       await render(SharedAccountPageComponent, {
         inputs: {
@@ -67,6 +63,33 @@ describe('SharedAccountPageComponent', () => {
       });
 
       expect(screen.getByText(headingText).textContent).toBe(headingText);
+      expect(mockUserAuthenticationStore.isLoading).toHaveBeenCalled();
+    });
+  });
+
+  describe('Spectator Integration Tests', () => {
+    let spectator: Spectator<SharedAccountPageComponent>;
+    const createComponent = createRoutingFactory({
+      component: SharedAccountPageComponent,
+      detectChanges: false,
+    });
+
+    beforeEach(() => {
+      mockUserAuthenticationStore = {
+        isLoading: jasmine.createSpy('isLoading').and.returnValue(false),
+      };
+      spectator = createComponent({providers: [{
+        provide: UserAuthenticationStore,
+        useValue: mockUserAuthenticationStore,
+      }]});
+    });
+
+    it('should have text same as input', () => {
+      const headingText = faker.lorem.words();
+      spectator.setInput('headingText', headingText);
+      spectator.detectChanges();
+      expect(spectator.query('h2')).toHaveText(headingText);
+      expect(mockUserAuthenticationStore.isLoading).toHaveBeenCalled();
     });
   });
 });
