d7ff99c81aa32443f878d56d7b3c295902de98a6
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-14 23:23:29 +0000

    Update Supabase key for staging environment
    Replaced the expired Supabase key with a new one in the staging environment configuration file. This ensures uninterrupted access to the Supabase service during development and testing.

--- kavindra-ui/src/environments/environment.stage.ts
+++ kavindra-ui/src/environments/environment.stage.ts
@@ -6,7 +6,7 @@ export const environment = {
   API_ENVIRONMENT: 'staging',
   SUPABASE_URL: 'https://auth.staging.kavindra.io',
   SUPABASE_KEY:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0MTM0NDAwLAogICJleHAiOiAxODkxOTAwODAwCn0.i5NEKQUZLvhB0hU44Uma0dcHKA4zaNSj-Hf5XG3gM1w',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0MTM0NDAwLAogICJleHAiOiAxODkxOTAwODAwCn0.pXf-akOzWGRZR8eF4l24D4RBmnwKcjq7qBoiQ-GWGEE',
   FLAGSMITH_CLIENT_SDK_KEY: 'Jw463yjab8T3oiqeDP7uWW',
   FLAGSMITH_API_URL: 'https://flagsmith.staging.kavindra.io/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
