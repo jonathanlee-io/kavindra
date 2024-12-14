b1f6455300456951951e809171ac8b742e078218
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-14 00:46:08 +0000

    Update Supabase key in production environment config
    Replaced the outdated Supabase key with a new one in the `environment.prod.ts` file. This ensures continued authentication and API functionality in the production environment. No other changes were made.

--- kavindra-ui/src/environments/environment.prod.ts
+++ kavindra-ui/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   API_ENVIRONMENT: 'production',
   SUPABASE_URL: 'https://auth.kavindra.io',
   SUPABASE_KEY:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucGVmeXNvdmV3Zm5hdmV1a2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1NjY3NDUsImV4cCI6MjA0NTE0Mjc0NX0.cyRHfAwWrSTiUtR80FYHJ0eCPFJgPKz3FdNo4UdVKac',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0MTM0NDAwLAogICJleHAiOiAxODkxOTAwODAwCn0.i5NEKQUZLvhB0hU44Uma0dcHKA4zaNSj-Hf5XG3gM1w',
   FLAGSMITH_CLIENT_SDK_KEY: '6zrfkbuWjUXwyNTKdFWqPD',
   FLAGSMITH_API_URL: 'https://flagsmith.kavindra.io/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
