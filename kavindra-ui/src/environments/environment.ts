741c2a339bf302dda24f52cd703ee9c85bb065b1
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-15 01:21:40 +0000

    Update Supabase key in environment configuration
    Replaced the expired Supabase key with a new one to ensure continued functionality of API authentication and services. No other changes were made to the environment configuration.

--- kavindra-ui/src/environments/environment.ts
+++ kavindra-ui/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   WIDGET_URL: 'http://localhost:8080/widget.js',
   SUPABASE_URL: 'https://auth.local.kavindra.io',
   SUPABASE_KEY:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0MTM0NDAwLAogICJleHAiOiAxODkxOTAwODAwCn0.i5NEKQUZLvhB0hU44Uma0dcHKA4zaNSj-Hf5XG3gM1w',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0MjIwODAwLAogICJleHAiOiAxODkxOTg3MjAwCn0.4lndNSCHAHPhWdVxW1V1QxNYf80ht7_HvTUlcTy0gEk',
   FLAGSMITH_CLIENT_SDK_KEY: 'ENp3D3xdLsQQe9brcjg3NF',
   FLAGSMITH_API_URL: 'https://flagsmith.local.kavindra.io/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
