7d62f91867f5f4a16449ccc98e30efd8ec872ecc
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-19 03:31:17 +0000

    Update Supabase URL and public key in staging environment
    Replaced the SUPABASE_URL to use the updated staging domain and changed the SUPABASE_PUBLIC_KEY accordingly. These updates ensure alignment with the new staging configuration for authentication services.

--- kavindra-ui/src/environments/environment.stage.ts
+++ kavindra-ui/src/environments/environment.stage.ts
@@ -4,9 +4,9 @@ export const environment = {
   SNACKBAR_DURATION_MS: 5000,
   TOAST_DURATION_MS: 5000,
   API_ENVIRONMENT: 'staging',
-  SUPABASE_URL: 'https://auth.staging.kavindra.io',
+  SUPABASE_URL: 'https://auth.kavindra-staging.com',
   SUPABASE_PUBLIC_KEY:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0NTY2NDAwLAogICJleHAiOiAxODkyMzMyODAwCn0.SaXzucFs8YxNEpFKiSEXuCOLK4yeD8fJ_f-UrZBW1nQ',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0NTY2NDAwLAogICJleHAiOiAxODkyMzMyODAwCn0.EShkBT74cmfI7A613w7Op8RmJ5zPPhrNSuwL6hGdqbU',
   FLAGSMITH_CLIENT_SDK_KEY: 'Jw463yjab8T3oiqeDP7uWW',
   FLAGSMITH_API_URL: 'https://flagsmith.staging.kavindra.io/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
