6c19961195ef71f69d2787911da736fa857b98ad
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-19 04:19:50 +0000

    Update environment configs for staging and local environments
    Updated SUPABASE_URL and FLAGSMITH_API_URL in both staging and local environment files. Adjustments ensure proper connectivity with the updated staging API endpoints. This change aligns with recent infrastructure updates.

--- kavindra-ui/src/environments/environment.stage.ts
+++ kavindra-ui/src/environments/environment.stage.ts
@@ -7,8 +7,8 @@ export const environment = {
   SUPABASE_URL: 'https://auth.kavindra-staging.com',
   SUPABASE_PUBLIC_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0NTY2NDAwLAogICJleHAiOiAxODkyMzMyODAwCn0.EShkBT74cmfI7A613w7Op8RmJ5zPPhrNSuwL6hGdqbU',
-  FLAGSMITH_CLIENT_SDK_KEY: 'Jw463yjab8T3oiqeDP7uWW',
-  FLAGSMITH_API_URL: 'https://flagsmith.staging.kavindra.io/api/v1/',
+  FLAGSMITH_CLIENT_SDK_KEY: 'X59LerX2sBae2URoMvXdhH',
+  FLAGSMITH_API_URL: 'https://flagsmith.kavindra-staging.com/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
   APPLICATION_MESSAGES_SERVICE_BASE_URL: 'v1/application-messages',
   USERS_SERVICE_BASE_URL: 'v1/users',
