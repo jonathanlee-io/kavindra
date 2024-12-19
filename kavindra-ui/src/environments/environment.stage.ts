12cc0728d1a526b859cb8d539d26cef49e20c486
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-19 23:54:33 +0000

    Update FLAGSMITH_CLIENT_SDK_KEY in staging environment
    Replaced the FLAGSMITH_CLIENT_SDK_KEY with a new value in the staging environment configuration file. This update ensures proper communication with the Flagsmith service using the latest credentials.

--- kavindra-ui/src/environments/environment.stage.ts
+++ kavindra-ui/src/environments/environment.stage.ts
@@ -7,7 +7,7 @@ export const environment = {
   SUPABASE_URL: 'https://auth.kavindra-staging.com',
   SUPABASE_PUBLIC_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogInNlcnZpY2Vfcm9sZSIsCiAgImlzcyI6ICJzdXBhYmFzZSIsCiAgImlhdCI6IDE3MzQ1NjY0MDAsCiAgImV4cCI6IDE4OTIzMzI4MDAKfQ.vI6GY9pKCTgQmrebLvfLYq2C01vMp-aHOuFeO_WkjiY',
-  FLAGSMITH_CLIENT_SDK_KEY: 'X59LerX2sBae2URoMvXdhH',
+  FLAGSMITH_CLIENT_SDK_KEY: 'ha8THs3YsZudtReyYAJbyz',
   FLAGSMITH_API_URL: 'https://flagsmith.kavindra-staging.com/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
   APPLICATION_MESSAGES_SERVICE_BASE_URL: 'v1/application-messages',
