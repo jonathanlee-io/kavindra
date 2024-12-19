2436d0ce095aa430378d6a50f1e488b1d33c0375
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-19 02:55:31 +0000

    Update staging environment config and feedback widget URL
    Updated Supabase public key in the staging environment file to renew the token expiration. Modified the feedback widget script URL to point to the local domain for development purposes.

--- kavindra-ui/src/environments/environment.stage.ts
+++ kavindra-ui/src/environments/environment.stage.ts
@@ -6,7 +6,7 @@ export const environment = {
   API_ENVIRONMENT: 'staging',
   SUPABASE_URL: 'https://auth.staging.kavindra.io',
   SUPABASE_PUBLIC_KEY:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0MTM0NDAwLAogICJleHAiOiAxODkxOTAwODAwCn0.pXf-akOzWGRZR8eF4l24D4RBmnwKcjq7qBoiQ-GWGEE',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM0NTY2NDAwLAogICJleHAiOiAxODkyMzMyODAwCn0.SaXzucFs8YxNEpFKiSEXuCOLK4yeD8fJ_f-UrZBW1nQ',
   FLAGSMITH_CLIENT_SDK_KEY: 'Jw463yjab8T3oiqeDP7uWW',
   FLAGSMITH_API_URL: 'https://flagsmith.staging.kavindra.io/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
