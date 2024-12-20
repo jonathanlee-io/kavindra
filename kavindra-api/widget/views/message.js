92be4e6bc8cb799106f963c7b0fa6315a1b4474a
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-20 02:30:40 +0000

    Refactor modal menu behavior and update overlay styles.
    Improve modal menu functionality by modifying event callbacks and cloning elements during the close action to preserve state. Update modal overlay and dialog styling to ensure consistency and fix display issues.

--- kavindra-api/widget/views/message.js
+++ kavindra-api/widget/views/message.js
@@ -5,20 +5,41 @@ const elements = [];
 let body;
 let project;
 
+export function closeModalMenuCallback(_event) {
+  closeModalMenu();
+}
+
+export function openModalMenuCallback(_event) {
+  openModalMenu();
+}
+
 export function closeModalMenu() {
   const modal = document.getElementsByClassName('js-widget-modal-menu')[0];
   modal.style.display = 'none'
+
+  const oldWidgetOverlay = document.getElementsByClassName('js-widget-overlay')[0];
+  const oldWidgetDialog = document.getElementsByClassName('js-widget-dialog')[0];
+  const widgetOverlay = oldWidgetOverlay.cloneNode(true);
+  const widgetDialog = oldWidgetDialog.cloneNode(true);
+  widgetOverlay.addEventListener('click', (_event) => openModalMenuCallback(_event));
+  widgetDialog.addEventListener('click', (_event) => openModalMenuCallback(_event));
+  oldWidgetOverlay.parentNode.replaceChild(widgetOverlay, oldWidgetOverlay);
+  oldWidgetDialog.parentNode.replaceChild(widgetDialog, oldWidgetDialog);
 }
 
 export function openModalMenu() {
   const modal = document.getElementsByClassName('js-widget-modal-menu')[0];
-  const modalContent = document.getElementsByClassName('js-widget-modal-menu-content')[0];
-  modal.style.display = 'block';
-  modalContent.style.display = 'block';
+  const modalContent = document.getElementsByClassName('js-widget-modal-content')[0];
+  modal.style.display = 'flex';
+  modalContent.style.display = 'flex';
   const modalCloseButton = document.getElementById('js-widget-modal-menu-close-button');
+  const widgetOverlay = document.getElementsByClassName('js-widget-overlay')[0];
+  const widgetDialog = document.getElementsByClassName('js-widget-dialog')[0];
   setTimeout(() => {
     modalCloseButton.addEventListener('click', closeModalMenu);
-  }, 500);
+    widgetOverlay.addEventListener('click', (_event) => closeModalMenu());
+    widgetDialog.addEventListener('click', (_event) => closeModalMenu());
+  }, 300);
   if (project.isBugReportsEnabled) {
     const modalReportButton = document.getElementById('js-widget-modal-menu-bug-button');
     modalReportButton.addEventListener('click', (_event) => openInnerModalMenu('js-widget-modal-bug-report', 'js-widget-modal-bug-report-close-button'));
@@ -69,6 +90,6 @@ export function show(params) {
 
   project = params.project;
 
-  widgetOverlay.addEventListener('click', (_event) => openModalMenu());
-  widgetDialog.addEventListener('click', (_event) => openModalMenu());
+  widgetOverlay.addEventListener('click', (_event) => openModalMenuCallback(_event));
+  widgetDialog.addEventListener('click', (_event) => openModalMenuCallback(_event));
 }
