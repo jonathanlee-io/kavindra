447aa5e8fb9865a1b88765ad8ee5f30fe290bc57
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-28 16:17:15 +0000

    Reduce default container stop delay in integration tests
    Lowered the `defaultIntegrationTestContainerStopDelay` from 10 seconds to 5 seconds to speed up test execution. This change improves the efficiency of integration tests without affecting reliability.

--- kavindra-api/src/lib/constants/testing/integration-testing.constants.ts
+++ kavindra-api/src/lib/constants/testing/integration-testing.constants.ts
@@ -1,3 +1,3 @@
 export const jestIntegrationTestTimeout = 120_000;
 export const e2eTestTimeout = 120_000;
-export const defaultIntegrationTestContainerStopDelay = 10_000;
+export const defaultIntegrationTestContainerStopDelay = 5_000;
