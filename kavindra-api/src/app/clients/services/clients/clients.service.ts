fe017d1cec9c6545349e6589ace6502860b9e806
Jonathan Lee <43010048+jonathan-lee-devel@users.noreply.github.com>
2024-12-31 17:33:53 +0000

    Add email support and client member management (#21)
    * Enable format on save with ESLint in Fleet.
    
    Added a Fleet settings file to ensure automatic formatting with ESLint on save for Node.js projects. This helps maintain consistent code style across the project.
    
    * Refactor logging to use static Logger methods
    
    Replaced instance-based logging with static Logger methods in the bootstrap function. This simplifies the logging process and removes unnecessary instantiations of the Logger class.
    
    * Add user email support and client member management
    
    Introduce an `email` field to the `User` schema with uniqueness enforced, and update the database migrations accordingly. Implement services and endpoints for adding and removing client members using their email addresses, enhancing client member management functionality.

--- kavindra-api/src/app/clients/services/clients/clients.service.ts
+++ kavindra-api/src/app/clients/services/clients/clients.service.ts
@@ -12,6 +12,7 @@ import {supabaseUserIdKey} from '../../../../lib/constants/auth/supabase-user-id
 import {reservedSubdomains} from '../../../../lib/constants/subdomains/reserved-subdomains.constants';
 import {POSTSuccessDto} from '../../../../lib/dto/POSTSuccess.dto';
 import {PaymentsService} from '../../../payments/services/payments/payments.service';
+import {UsersRepositoryService} from '../../../users/repositories/users-repository/users-repository.service';
 import {CreateClientDto} from '../../dto/CreateClient.dto';
 import {IsSubdomainAvailableDto} from '../../dto/IsSubdomainAvailable.dto';
 import {ClientsRepositoryService} from '../../repositories/clients-repository/clients-repository.service';
@@ -20,6 +21,7 @@ import {ClientsRepositoryService} from '../../repositories/clients-repository/cl
 export class ClientsService {
   constructor(
     private readonly clientsRepository: ClientsRepositoryService,
+    private readonly usersRepository: UsersRepositoryService,
     private readonly logger: Logger,
   ) {}
 
@@ -108,5 +110,61 @@ export class ClientsService {
       isIncludeAdmins: true,
       isIncludeProjects: true,
     });
+  }
+
+  async removeMemberFromClientById(
+    currentUser: AuthUser,
+    clientId: string,
+    emailToRemove: string,
+  ) {
+    const client = await this.getClientForModification(currentUser, clientId);
+    if (!client.members.map((member) => member.email).includes(emailToRemove)) {
+      throw new BadRequestException(
+        `Could not find member with email: ${emailToRemove}`,
+      );
+    }
+    return this.clientsRepository.removeMemberFromClientById(
+      clientId,
+      emailToRemove,
+    );
+  }
+
+  async addMemberFromClientById(
+    currentUser: AuthUser,
+    clientId: string,
+    emailToAdd: string,
+  ) {
+    const client = await this.getClientForModification(currentUser, clientId);
+    if (client.members.map((member) => member.email).includes(emailToAdd)) {
+      throw new BadRequestException(
+        `Member with email: ${emailToAdd} already exists`,
+      );
+    }
+    const userToAdd = await this.usersRepository.findByEmail(emailToAdd);
+    if (!userToAdd) {
+      throw new BadRequestException(
+        `Could not find user with email: ${emailToAdd}`,
+      );
+    }
+    return this.clientsRepository.addMemberToClientById(clientId, emailToAdd);
+  }
+
+  private async getClientForModification(
+    currentUser: AuthUser,
+    clientId: string,
+  ) {
+    const client = await this.clientsRepository.getClientById(clientId, {
+      isIncludeAdmins: true,
+      isIncludeMembers: true,
+    });
+    if (!client) {
+      throw new NotFoundException(`Could not find client with id: ${clientId}`);
+    }
+    if (
+      !client.admins.map((admin) => admin.email).includes(currentUser.email)
+    ) {
+      throw new ForbiddenException();
+    }
+    return client;
   }
 }
