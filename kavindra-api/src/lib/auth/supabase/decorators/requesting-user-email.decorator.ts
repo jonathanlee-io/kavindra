bff96f350fba09051c421b38805b228f05262d6d
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-01-01 19:38:10 +0000

    Remove Supabase authentication and related components
    Eliminated Supabase-based authentication, strategies, guards, and utilities from the project. Replaced client identification methods with custom request headers and removed unused configurations and dependencies to streamline the authentication process.

--- kavindra-api/src/lib/auth/supabase/decorators/requesting-user-email.decorator.ts
+++
@@ -1,9 +0,0 @@
-import {createParamDecorator, ExecutionContext} from '@nestjs/common';
-
-export const RequestingUserEmail = createParamDecorator(
-  (_data: unknown, context: ExecutionContext) => {
-    return context.switchToHttp().getRequest().headers[
-      'X-Requesting-User-Email'.toLowerCase()
-    ];
-  },
-);
