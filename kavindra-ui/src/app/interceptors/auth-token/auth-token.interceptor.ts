2e7eb7ff5b53a863809f3fc49b30a406a4a4074d
Jonathan Lee <43010048+jonathan-lee-devel@users.noreply.github.com>
2025-01-03 22:44:26 +0000

    Refactor code for improved logic handling and error management (#27)
    * Refactor feature flag error handling in guard logic
    
    Replaced redundant login error handling with a dedicated method for disabled feature access attempts in the FeatureFlagsStore. This improves clarity and centralizes error logging for better maintainability. Simplified imports for cleaner code structure.
    
    * Refactor auth-token logic to handle missing access token.
    
    Simplifies conditional logic by ensuring requests without an access token are forwarded as-is. This reduces unnecessary cloning of requests and improves code readability. No changes to functionality or external behavior.

--- kavindra-ui/src/app/interceptors/auth-token/auth-token.interceptor.ts
+++ kavindra-ui/src/app/interceptors/auth-token/auth-token.interceptor.ts
@@ -14,15 +14,17 @@ export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
     /(http):\/\/localhost:8080\/(.*)/.test(req.url) ||
     new RegExp('/(https)://(.*).' + tenantStore.customHostname() + '/').test(
         req.url,
-    ) && supabaseService.session?.access_token
+    )
   ) {
     return next(
+      (supabaseService.session?.access_token) ?
         req.clone({
           withCredentials: true,
           setHeaders: {
             Authorization: `Bearer ${supabaseService.session?.access_token}`,
           },
-        }),
+        }) :
+        req,
     );
   }
 
