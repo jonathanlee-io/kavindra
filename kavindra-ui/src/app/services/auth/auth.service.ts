e4a895239c3f852818fef44248115e64e56b5739
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-01-11 00:24:06 +0000

    Refactor auth service and update Cypress config formatting
    Removed unused methods and dependencies from AuthService for cleaner and more maintainable code. Updated Cypress config formatting for consistency with style guidelines.

--- kavindra-ui/src/app/services/auth/auth.service.ts
+++ kavindra-ui/src/app/services/auth/auth.service.ts
@@ -1,20 +1,13 @@
 import {HttpClient} from '@angular/common/http';
 import {inject, Injectable} from '@angular/core';
-import {Router} from '@angular/router';
 
 import {TenantStore} from '../../+state/tenant/tenant.store';
-import {RoutePath} from '../../app.routes';
-import {rebaseRoutePath, RouterUtils} from '../../util/router/Router.utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  static readonly darkModeKey = 'dark-mode';
-  static readonly nextParam = 'next';
-
   private readonly httpClient = inject(HttpClient);
-  private readonly router = inject(Router);
   private readonly tenantStore = inject(TenantStore);
 
   checkIn() {
@@ -22,39 +15,5 @@ export class AuthService {
         this.tenantStore.getFullRequestUrl('v1/users/authenticated/check-in'),
         {},
     );
-  }
-
-  public redirectIfNotAnonymous() {
-    if (this.router.url !== '/') {
-      // Don't redirect to login page on anonymous pages (first-time visit etc.)
-      this.router
-          .navigate([rebaseRoutePath(RoutePath.LOGIN)])
-          .catch(RouterUtils.navigateCatchErrorCallback);
-    }
-  }
-
-  public getNextParamFromLocalStorageAndNoReset() {
-    return null;
-  }
-
-  public setNextParamInLocalStorageIfNotAnonymous(next: string | null) {
-    console.log(next);
-    localStorage.removeItem(AuthService.nextParam);
-    return;
-    // if (
-    //   next?.startsWith('/#') ||
-    //   next === rebaseRoutePath(RoutePath.LANDING_PAGE) ||
-    //   next === rebaseRoutePath(RoutePath.HOME)
-    // ) {
-    //   return;
-    // }
-    // if (next && next !== '/' && next !== rebaseRoutePath(RoutePath.LOGIN)) {
-    //   localStorage.setItem(AuthService.nextParam, next);
-    // } else {
-    //   localStorage.setItem(
-    //     AuthService.nextParam,
-    //     rebaseRoutePath(RoutePath.HOME),
-    //   );
-    // }
   }
 }
