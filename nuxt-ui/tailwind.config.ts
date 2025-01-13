1f5c5821abc0963cca505045f3f0989b8e9db122
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-01-13 02:34:52 +0000

    Remove custom 'meteor' animation from Tailwind config
    The 'meteor' animation and its keyframes have been removed from the Tailwind CSS configuration file. This simplifies the configuration and removes unused or unnecessary code. All other configurations remain unchanged.

--- nuxt-ui/tailwind.config.ts
+++ nuxt-ui/tailwind.config.ts
@@ -5,19 +5,6 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
-      animation: {
-        meteor: "meteor 5s linear infinite",
-      },
-      keyframes: {
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
-          "70%": { opacity: 1 },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: 0,
-          },
-        }
-      },
       container: {
         center: true,
         padding: {
