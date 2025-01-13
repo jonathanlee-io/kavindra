82622a7859ab88992e4ad9bbb8c81ded404c9e71
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-01-13 06:26:16 +0000

    Add Plausible Analytics script to head configuration
    This update integrates Plausible Analytics by adding its script to the head configuration in the Nuxt app. The script is set to load asynchronously and is configured for tracking features like file downloads and outbound links.

--- nuxt-ui/nuxt.config.ts
+++ nuxt-ui/nuxt.config.ts
@@ -21,7 +21,10 @@ export default defineNuxtConfig({
     },
     app: {
       head: {
-        script: [{ src: "https://www.api.kavindra.io/v1/scripts/feedback-widget.js" }],
+        script: [
+          { src: 'https://www.api.kavindra.io/v1/scripts/feedback-widget.js' },
+          { src: 'https://plausible.io/js/script.file-downloads.outbound-links.pageview-props.revenue.tagged-events.js', async: true, 'data-domain': 'kavindra.io'}
+        ],
       },
       pageTransition: { name: "page", mode: "out-in" },
     },
